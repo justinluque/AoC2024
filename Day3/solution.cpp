656300b3deea8acba85113bb838b2d21ae4a81bd
Justin Luque <justin.luque@gmail.com>
2024-12-10 18:31:45 -0500

    feat: implement part 2 of day 3 solution

--- Day3/solution.cpp
+++ Day3/solution.cpp
@@ -6,25 +6,66 @@
 #include <algorithm>
 #include <regex>
 
-int main()
+int computeSumOfMultiplication(const std::string &data)
 {
-  std::string input = AOCHelper::readInput("Day3/input.txt");
-
-  std::regex pattern(R"(mul\((\d+),(\d+)\))");
-  std::smatch match;
+  const std::regex pattern(R"(mul\((\d+),(\d+)\))");
+  std::smatch matches;
+  auto searchBegin = data.cbegin();
   int sumOfMultiplications = 0;
 
-  auto begin = input.cbegin();
-  auto end = input.cend();
-
-  while (std::regex_search(begin, end, match, pattern))
+  while (std::regex_search(searchBegin, data.cend(), matches, pattern))
   {
-    int firstNum = std::stoi(match[1].str());
-    int secondNum = std::stoi(match[2].str());
+    int firstNum = std::stoi(matches[1].str());
+    int secondNum = std::stoi(matches[2].str());
 
     sumOfMultiplications += firstNum * secondNum;
-    begin = match.suffix().first;
+    searchBegin = matches.suffix().first;
   }
 
-  std::cout << sumOfMultiplications << std::endl;
+  return sumOfMultiplications;
+}
+
+int computeConditionalSumOfMultiplication(const std::string &data)
+{
+  const std::regex pattern(R"((don't\(\))|(do\(\))|mul\((\d+),(\d+)\))");
+
+  std::smatch matches;
+  auto searchBegin = data.cbegin();
+  int sumOfMultiplications = 0;
+  bool calculate = true;
+
+  while (std::regex_search(searchBegin, data.cend(), matches, pattern))
+  {
+    if (matches[1].matched) // Match for "don't()"
+    {
+      calculate = false;
+    }
+    else if (matches[2].matched) // Match for "do()"
+    {
+      calculate = true;
+    }
+    else if (calculate && matches[3].matched && matches[4].matched) // Match for "mul(x,y)"
+    {
+      int firstNum = std::stoi(matches[3].str());
+      int secondNum = std::stoi(matches[4].str());
+      sumOfMultiplications += firstNum * secondNum;
+    }
+
+    searchBegin = matches.suffix().first;
+  }
+
+  return sumOfMultiplications;
+}
+
+int main()
+{
+  const std::string input = AOCHelper::readInput("Day3/input.txt");
+
+  const int sumOfMultiplications = computeSumOfMultiplication(input);
+  const int sumOfMultiplicationsProcessed = computeConditionalSumOfMultiplication(input);
+
+  std::cout << "Part 1: " << sumOfMultiplications << '\n';
+  std::cout << "Part 2: " << sumOfMultiplicationsProcessed << '\n';
+
+  return 0;
 }
