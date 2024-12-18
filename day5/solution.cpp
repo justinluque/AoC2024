6b4dba1f17a3248c7d9b419c8c05c8efee7a08c1
Justin Luque <justin.luque@gmail.com>
2024-12-18 02:07:14 -0500

    feat: implement part 2 of day 5 solution

--- day5/solution.cpp
+++ day5/solution.cpp
@@ -2,11 +2,19 @@
 
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
+
+void printUpdate(const std::vector<int> &update) {
+  for (int num : update)
+    std::cout << num << ' ';
+  std::cout << '\n';
+}
 
 std::vector<std::pair<int, int>> extractRules(const std::string &input) {
   std::vector<std::pair<int, int>> rules;
@@ -69,7 +77,7 @@ parseRules(const std::vector<std::pair<int, int>> &rules) {
 
 bool isCorrectOrder(const std::vector<int> &update,
                     const std::vector<std::pair<int, int>> &rules) {
-  std::unordered_map<int, std::vector<int>> adj = parseRules(rules);
+  const std::unordered_map<int, std::vector<int>> adj = parseRules(rules);
 
   for (size_t i = 0; i < update.size(); i++) {
     int job = update[i];
@@ -86,6 +94,70 @@ bool isCorrectOrder(const std::vector<int> &update,
   }
 
   return true;
+}
+
+// xth indegree: number of jobs that must occur before the xth job
+std::unordered_map<int, int>
+calculateInDegrees(const std::vector<int> &update,
+                   const std::unordered_map<int, std::vector<int>> &rules) {
+  std::unordered_map<int, int> inDegree;
+
+  for (int job : update)
+    inDegree[job] = 0;
+
+  for (const auto &[job, dependencies] : rules) {
+    for (int dependentJob : dependencies) {
+      if (inDegree.contains(dependentJob) && inDegree.contains(job))
+        inDegree[dependentJob]++;
+    }
+  }
+
+  return inDegree;
+}
+
+std::vector<int>
+topologicalSort(const std::vector<int> &update,
+                const std::vector<std::pair<int, int>> &rules) {
+  const std::unordered_map<int, std::vector<int>> adj = parseRules(rules);
+  std::unordered_map<int, int> inDegree = calculateInDegrees(update, adj);
+  std::queue<int> jobQueue;
+  std::vector<int> sorted;
+
+  for (const auto &[key, value] : inDegree)
+    std::cout << key << " is depended on by " << value << " jobs.\n";
+
+  for (int num : update) {
+    if (inDegree.at(num) == 0)
+      jobQueue.push(num);
+  }
+
+  while (!jobQueue.empty()) {
+    int job = jobQueue.front();
+    jobQueue.pop();
+    sorted.push_back(job);
+
+    if (adj.contains(job)) {
+      for (int neighbor : adj.at(job)) {
+        if (inDegree.contains(neighbor)) {
+          inDegree[neighbor]--;
+
+          if (inDegree[neighbor] == 0)
+            jobQueue.push(neighbor);
+        }
+      }
+    }
+  }
+
+  std::cout << "Unsorted: ";
+  printUpdate(update);
+  std::cout << "Sorted: ";
+  printUpdate(sorted);
+
+  if (sorted.size() != update.size()) {
+    throw std::domain_error("Could not topologically sort, cycle detected");
+  }
+
+  return sorted;
 }
 
 int getMiddlePageNumber(const std::vector<int> &update) {
@@ -109,14 +181,31 @@ int sumMiddlePagesOfCorrectReports(
   return sum;
 }
 
+int sumMiddlePagesOfSortedReports(
+    const std::vector<std::vector<int>> &updates,
+    const std::vector<std::pair<int, int>> &rules) {
+
+  int sum = 0;
+  for (const std::vector<int> &update : updates) {
+    if (!isCorrectOrder(update, rules)) {
+      std::vector<int> sorted = topologicalSort(update, rules);
+      sum += getMiddlePageNumber(sorted);
+    }
+  }
+
+  return sum;
+}
+
 int main() {
   std::string input = AOCHelper::readInput("Day5/input.txt");
   std::vector<std::pair<int, int>> rules = extractRules(input);
   std::vector<std::vector<int>> updates = extractUpdates(input);
 
-  int sum = sumMiddlePagesOfCorrectReports(updates, rules);
+  int sumOfCorrectMiddlePages = sumMiddlePagesOfCorrectReports(updates, rules);
+  int sumOfsortedMiddlePages = sumMiddlePagesOfSortedReports(updates, rules);
 
-  std::cout << "Part 1: " << sum << '\n';
+  std::cout << "Part 1: " << sumOfCorrectMiddlePages << '\n';
+  std::cout << "Part 2:" << sumOfsortedMiddlePages << '\n';
 
   return 0;
 }
